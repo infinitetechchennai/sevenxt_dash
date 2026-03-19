017f0b7bc0caaf49b55dc0ff249550087cf65a5d
unknown <infinitetechchennai@gmail.com>
2026-03-19 14:11:57 +0530

    fix index.tsx

--- Frontend/index.tsx
+++ Frontend/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css'; // make sure this exists
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/login">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
