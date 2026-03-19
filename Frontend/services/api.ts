29eb6e1f0635165aa7d652dc9a4a492e644dc960
unknown <infinitetechchennai@gmail.com>
2026-03-19 16:58:03 +0530

    modified env localhost

--- Frontend/services/api.ts
+++ Frontend/services/api.ts
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL || "http://localhost:8001";
-export const API_BASE_URL = "http://localhost:8001";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export interface LoginRequest {
   email: string;
   password: string;
