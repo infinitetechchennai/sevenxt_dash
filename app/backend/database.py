3ba467cddbb4aa848a71b2de4c9a7bb68e806ca9
Loges <loguloges778@gmail.com>
2026-03-20 17:50:19 +0530

    fix database

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,14 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def get_db_connection():
+    return engine.raw_connection()
