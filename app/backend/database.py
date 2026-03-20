840275267949845fe68a514f028c98bbc7a90b54
Loges <loguloges778@gmail.com>
2026-03-20 17:37:32 +0530

    fixed database.py

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,15 +1,14 @@
-import mysql.connector
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def get_db_connection():
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+def get_db():
+    db = SessionLocal()
     try:
-        return mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME"),
-            port=int(os.getenv("DB_PORT", 3306))
-        )
-    except Exception as e:
-        print(f"Database connection error: {e}")
-        raise Exception("Failed to connect to the database")
+        yield db
+    finally:
+        db.close()
