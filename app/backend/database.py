a24dbb670face7d1927c8f9081fb21ab9390df05
Loges <loguloges778@gmail.com>
2026-03-20 17:24:36 +0530

    fix database

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,19 +1,15 @@
 import mysql.connector
-from mysql.connector import Error
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_db_connection():
     try:
         return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="sevenext",
-            port=3306
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT", 3306))
         )
-    except Error as e:
+    except Exception as e:
         print(f"Database connection error: {e}")
         raise Exception("Failed to connect to the database")
